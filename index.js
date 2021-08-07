20e3b0dd508a706be037d30f359390962097741f
Vasilev <lyubimir.vasilev@hpe.com>
2021-08-07 18:08:56 +0200

    Finish header and footer hbs

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
+//Seting variable for the core modules 
 const path = require("path");
 const fs = require("fs");
 
+//Seting variables for express/express libraries
 const express = require("express");
 const bodyParser = require("body-parser");
 const expressHbs = require("express-handlebars");
 
 const envValues = require("./envVal");
 
+//Piping routers from their dirs
 const adminRouter = require("./routes/admin");
 const homeRouter = require("./routes/home");
 const userRouter = require("./routes/users");
 
+
+//Piping controller for the products
 const productsController = require("./controllers/products");
 
 
 const app = express();
 
 
+//Calibrating view-engine's(Handlebars) extension type and directory path
 app.engine("hbs", expressHbs({
     layoutsDir: 'views/layouts/',
     defaultLayout: 'main-layout',
-    partialsDir: 'views/base',
+    partialsDir: ['views/base', 'views/shop'],
     extname: "hbs"
 }));
 
 
-
-
-
+//Setting the view egine type and pathing
 app.set("view engine", "hbs");
 app.set("views", "views");
 
+
+//Adjusting body parsing and delivery of static css files for every render
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+//Calling diferent routers
 app.use(homeRouter);
 app.use("/admin", adminRouter);
 app.use(userRouter);
@@ -41,6 +48,7 @@ app.use(userRouter);
 app.use(productsController.pageNotFound);
 
 
+//Core server deployment
 app.listen(envValues.port, () => {
     console.log(`Server is runing on port ${envValues.port}.`);
 });
